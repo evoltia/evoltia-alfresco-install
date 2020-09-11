69aae01006f981677415d67c429b9acd03338d40
Nacho <nacho.manzano@gmail.com>
2020-09-11 09:04:09 +0200

    Update firewall.sh
    Abrir puerto 9000 para la capa de servicios

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -2,5 +2,6 @@
 firewall-cmd --zone=public --permanent --add-service=http
 firewall-cmd --zone=public --permanent --add-port 8080/tcp
 firewall-cmd --zone=public --permanent --add-port 8983/tcp
+firewall-cmd --zone=public --permanent --add-port 9000/tcp
 firewall-cmd --reload
 firewall-cmd --list-all
